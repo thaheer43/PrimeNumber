981ac2e94b1c9e7af6d954a83501c772615f2ab7
“thaheer” <thaheer43@gmail.com>
2022-05-22 23:00:16 +0530

    Problem three: Prime Number Logic updated

--- src/IsPrime.java
+++ src/IsPrime.java
@@ -1,4 +1,32 @@
 
-public class IsPrime {
+import java.util.Scanner;
+
+public class isPrime {
+	
+	static boolean isPrime(int num)
+	{
+	boolean isPrime=true;
+	for(int i=2; i<=num/2 ; i++   )      
+    {
+            if(num % i == 0)
+            {
+                isPrime=false;
+                break;
+            }
+    }
+            return isPrime;
+	}
+	public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num=scanner.nextInt();
+        boolean result=isPrime(num);
+        if(result)
+            System.out.println(num+" is Prime");
+             else
+              System.out.println(num+"is not Prime");
+          
+        }	
 
 }
+
